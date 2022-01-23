8a6adb93b99880afd8ea6e55de59259df3d918bc
Oleg <>
2022-01-23 12:41:05 +0400

    Добавьте файлы проекта.

---
+++ LibrerySimbols/ClassSimbols.cs
@@ -0,0 +1,61 @@
+﻿namespace LibrerySimbols
+{
+    public class StringIsCollectionChars
+    {
+        public char[] MassChars;
+        public StringIsCollectionChars(char[] myString) => MassChars = myString;
+        public bool IsDifferentOrEquals(char[] firstString, char[] otherString)
+        {
+            if (firstString.Length != otherString.Length)
+                return false;
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                if (firstString[i] != otherString[i])
+                return false;
+            }
+            return true;
+        }
+        public char[] ConcatStrings(StringIsCollectionChars secondString)
+        {
+            var res = new char[MassChars.Length + secondString.MassChars.Length];
+            MassChars.CopyTo(res, 0);
+            secondString.MassChars.CopyTo(res, MassChars.Length);
+            return res;
+        }
+        public int LookingForSymbol(char c)
+        {
+            for (int i = 0; i < MassChars.Length; i++)
+            {
+                if (c == MassChars[i])
+                    return i;
+            }
+            return -1;
+        }
+        public char[] ToCharMass()
+        {
+            char[] arr = new char[MassChars.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = MassChars[i];
+            }
+            return arr;
+        }
+        public override string ToString() => new string(MassChars);
+        public static double ToDouble(StringIsCollectionChars convertString)
+        {
+            double result = Convert.ToDouble(convertString.ToString());
+            return result;
+        }
+        public char this[int index]
+        {
+            get
+            {
+                return MassChars[index];
+            }
+            set
+            {
+                MassChars[index] = value;
+            }
+        }
+    }
+}
---
+++ LibrerySimbols/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace LibrerySimbols 
+{
+    class Program 
+{
+        static void Main(string[] args)
+        {
+            char[] newYourChars = new char[12] { 'H', 'E', 'L', 'L', 'O', ',', 'W', 'O', 'R', 'L', 'D', '!'};
+            char[] otherYourChars = new char[2] { '!' , '!' };
+
+            StringIsCollectionChars firstChars = new StringIsCollectionChars(newYourChars);
+            StringIsCollectionChars secondChars = new StringIsCollectionChars(otherYourChars);
+            StringIsCollectionChars concatChars = new StringIsCollectionChars(firstChars.ConcatStrings(secondChars));
+
+            Console.WriteLine(concatChars.ToString());
+            Console.WriteLine(firstChars[6]);
+            Console.WriteLine(firstChars[7]);
+            Console.WriteLine(firstChars[8]);
+            Console.WriteLine(firstChars[9]);
+            Console.WriteLine(firstChars[10]);
+        }
+    }
+}
